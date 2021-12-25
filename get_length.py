338cdcf13cb377b0a94bb2631f60b17d6aa65e7a
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-12-25 20:26:23 +0500

    task 01

--- get_length.py
+++ get_length.py
@@ -9,8 +9,20 @@ def get_length(num):
     Returns:
         int: length of integer
     """
+    x1=num%10
+    num//=10
+    x2=num%10
+    num//=10
+    x3=num%10
+    num//=10
+    x4=num%10
+    num//=10
+    x5=num%10
+    num//10
+
+
     
     # return number of digits in integer
-    return 
+    return x1,x2,x3,x4,x5
 
  
