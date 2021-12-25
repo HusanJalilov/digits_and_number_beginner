8e2a6f6ae84bda502c324f76d2de81b872d9b162
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-12-25 20:08:05 +0500

    Task 2

--- get_sum_digits.py
+++ get_sum_digits.py
@@ -1,13 +1,35 @@
-#Find the sum of digits in an integer
-def get_sum_digits(num):
+#get number of digits in an int?
+def get_length(num):
     """
-    Get sum of digits in integer
-    
+    Get length of integer
+`   
     Args:
-        num (int): integer to get sum of digits of
-    
+        num (int): integer to get length of
+
     Returns:
-        int: sum of digits in integer
+        int: length of integer
     """
-    # return sum of digits in integer
-    return  
+    x1=num%10
+    num//=10
+    
+    x2=num%10
+    num//=10
+    
+    x3=num%10
+    num//=10
+    
+    x4=num%10
+    num//=10
+
+    x5=num%10
+    num//=10
+    s=0
+    s=x1+x2+x3+x4+x5
+
+    
+    
+    # return number of digits in integer
+    return s
+
+
+ 
